d20e41dd75f40839988b7e475e4a528eba93efab
princeGithub999 <prince85656@gmail.com>
2025-02-12 17:00:45 +0530

    deleted some pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const Scaffold(),
     );
   }
 }
