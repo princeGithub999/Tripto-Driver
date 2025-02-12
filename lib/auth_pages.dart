8db874924d0facd8ed8bb57b9ac59fa25e880a8b
Aakash@edugaon <162113252+aakashlionhouse@users.noreply.github.com>
2025-02-12 17:11:48 +0530

    creat auth page

---
+++ lib/auth_pages.dart
@@ -0,0 +1,17 @@
+class AuthPages extends StatefulWidget {
+  const AuthPages({super.key});
+
+  @override
+  State<AuthPages> createState() => _AuthPagesState();
+}
+
+class _AuthPagesState extends State<AuthPages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Placeholder {
+  const Placeholder();
+}
