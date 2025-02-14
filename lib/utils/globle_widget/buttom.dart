b72dceca6945a77a22f6a778406478fc3a87a272
princeGithub999 <prince85656@gmail.com>
2025-02-14 21:34:20 +0530

    i am doing google auth

--- lib/utils/globle_widget/buttom.dart
+++ lib/utils/globle_widget/buttom.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:tripto_driver/utils/constants/app_image.dart';
 import 'package:tripto_driver/utils/constants/colors.dart';
 
+import '../app_sizes/sizes.dart';
+
 class MyButton {
 
-  static Widget sendOtpButton(VoidCallback onPress) {
+  static Widget globalButton(VoidCallback onPress, String buttomName) {
     return ElevatedButton(
       onPressed: onPress,
       style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.blue900,
-          padding: EdgeInsets.symmetric(horizontal:130)
+          // padding: EdgeInsets.symmetric(horizontal:130),
+        minimumSize: const Size(double.infinity,40),
+
       ),
-      child: const Text('Send OTP',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
+      child:  Text(buttomName,style: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: AppSizes.buttomTextSize),),
     );
   }
 
@@ -21,7 +25,7 @@ class MyButton {
       onPressed: onPress,
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.blue900,
-        padding: const EdgeInsets.symmetric(horizontal: 90,),
+        minimumSize: const Size(double.infinity,40),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
         ),
@@ -35,9 +39,9 @@ class MyButton {
             width: 24,
           ),
            const SizedBox(width: 10), // Icon aur text ke beech gap
-          const Text(
+           Text(
             'Login with Google',
-            style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),
+            style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: AppSizes.buttomTextSize),
           ),
         ],
       ),
